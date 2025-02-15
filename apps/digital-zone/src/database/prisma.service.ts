2a3d61f810e7501b4ce23364476717508bad033d
Sergey Plotnikov <softispower@gmail.com>
2025-02-16 01:43:17 +0500

    Improve db connection and docker

--- apps/digital-zone/src/database/prisma.service.ts
+++ apps/digital-zone/src/database/prisma.service.ts
@@ -1,11 +1,22 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     async onModuleInit() {
-        await this.$connect();
+        let retries = 5;
+        while (retries > 0) {
+            try {
+                await this.$connect();
+                console.log('✅ Database connected successfully');
+                return;
+            } catch (error) {
+                console.error(`❌ Database connection failed. Retrying in 5s... (${retries} retries left)`);
+                retries--;
+                await new Promise(res => setTimeout(res, 5000));
+            }
+        }
+        throw new Error('Database connection failed after multiple retries.');
     }
 
     async onModuleDestroy() {
